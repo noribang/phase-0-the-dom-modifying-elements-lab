173a02123266e52a056a4fde5969411bad8647b8
hoonkim <hoonk9387@gmail.com>
2022-04-23 16:05:50 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+/* index.html
+after index.js is processed
+  1) no longer has DOM node 'main#main'
+  2) has a 'newHeader' variable that points to node 'h1#victory'
+  3) has a 'newHeader' variable that points to node 'h1#victory'
+  4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside */
+
+/* 1) no longer has DOM node 'main#main' */
+const main = document.querySelector('main#main')
+main.remove()
+/* 2) has a 'newHeader' variable that points to node 'h1#victory' */
+/* 3) has a 'newHeader' variable that points to node 'h1#victory' */
+const newHeader = document.querySelector('h1#victory')
+/* 4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside */
+newHeader.textContent = "YOUR-NAME is the champion"
+
